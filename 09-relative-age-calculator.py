f445114b704ef330bde68ae0d563f56e8c23e438
Miscellaneous786 <168644773+Miscellaneous786@users.noreply.github.com>
2024-09-12 20:22:56 +0500

    09-relative-age-calculator.py

--- 09-relative-age-calculator.py
+++ 09-relative-age-calculator.py
@@ -1,4 +1,14 @@
-age = input("Enter animal age: ")
-
+age = input("Enter animal age (in years): ")
 while age.isnumeric() == False:
     age = input("Invalid Input. Enter animal age: ")
+
+type = input("Enter 'd' for dog, 'c' for cat or 'e' for elephantEnter the type of animal: ")
+while type not in ["d", "c", "e", "dog", "cat", "elephant"]:
+    type = input("Invalid Input. Enter animal type: ")
+
+if type in ["d", "dog"]:
+    print(f"Equivalent human age: {7*age}")
+elif type in ["c", "cat"]:
+    print(f"Equivalent human age: {6*age}")
+elif type in ["e", "elephant"]:
+    print(f"Equivalent human age: {3*age}")
