dd30d0d2c09cfb58e988d84a75b7dedbe94d962f
Miscellaneous786 <168644773+Miscellaneous786@users.noreply.github.com>
2024-09-16 15:05:49 +0500

    10-connect-4-V1.3.py

--- 10-connect-4-V1.3.py
+++ 10-connect-4-V1.3.py
@@ -1,5 +1,6 @@
 import random
-list = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+list_of_entrances = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
 def print_board():
     board = f'''
  _______________________________________________________________
@@ -7,28 +8,28 @@ def print_board():
 |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[56]}   |   {list[57]}   |   {list[58]}   |   {list[59]}   |   {list[60]}   |   {list[61]}   |   {list[62]}   |   {list[63]}   |
+|   {list_of_entrances[56]}   |   {list_of_entrances[57]}   |   {list_of_entrances[58]}   |   {list_of_entrances[59]}   |   {list_of_entrances[60]}   |   {list_of_entrances[61]}   |   {list_of_entrances[62]}   |   {list_of_entrances[63]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[48]}   |   {list[49]}   |   {list[50]}   |   {list[51]}   |   {list[52]}   |   {list[53]}   |   {list[54]}   |   {list[55]}   |
+|   {list_of_entrances[48]}   |   {list_of_entrances[49]}   |   {list_of_entrances[50]}   |   {list_of_entrances[51]}   |   {list_of_entrances[52]}   |   {list_of_entrances[53]}   |   {list_of_entrances[54]}   |   {list_of_entrances[55]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[40]}   |   {list[41]}   |   {list[42]}   |   {list[43]}   |   {list[44]}   |   {list[45]}   |   {list[46]}   |   {list[47]}   |
+|   {list_of_entrances[40]}   |   {list_of_entrances[41]}   |   {list_of_entrances[42]}   |   {list_of_entrances[43]}   |   {list_of_entrances[44]}   |   {list_of_entrances[45]}   |   {list_of_entrances[46]}   |   {list_of_entrances[47]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[32]}   |   {list[33]}   |   {list[34]}   |   {list[35]}   |   {list[36]}   |   {list[37]}   |   {list[38]}   |   {list[39]}   |
+|   {list_of_entrances[32]}   |   {list_of_entrances[33]}   |   {list_of_entrances[34]}   |   {list_of_entrances[35]}   |   {list_of_entrances[36]}   |   {list_of_entrances[37]}   |   {list_of_entrances[38]}   |   {list_of_entrances[39]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[24]}   |   {list[25]}   |   {list[26]}   |   {list[27]}   |   {list[28]}   |   {list[29]}   |   {list[30]}   |   {list[31]}   |
+|   {list_of_entrances[24]}   |   {list_of_entrances[25]}   |   {list_of_entrances[26]}   |   {list_of_entrances[27]}   |   {list_of_entrances[28]}   |   {list_of_entrances[29]}   |   {list_of_entrances[30]}   |   {list_of_entrances[31]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[16]}   |   {list[17]}   |   {list[18]}   |   {list[19]}   |   {list[20]}   |   {list[21]}   |   {list[22]}   |   {list[23]}   |
+|   {list_of_entrances[16]}   |   {list_of_entrances[17]}   |   {list_of_entrances[18]}   |   {list_of_entrances[19]}   |   {list_of_entrances[20]}   |   {list_of_entrances[21]}   |   {list_of_entrances[22]}   |   {list_of_entrances[23]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[8]}   |   {list[9]}   |   {list[10]}   |   {list[11]}   |   {list[12]}   |   {list[13]}   |   {list[14]}   |   {list[15]}   |
+|   {list_of_entrances[8]}   |   {list_of_entrances[9]}   |   {list_of_entrances[10]}   |   {list_of_entrances[11]}   |   {list_of_entrances[12]}   |   {list_of_entrances[13]}   |   {list_of_entrances[14]}   |   {list_of_entrances[15]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 |       |       |       |       |       |       |       |       |
-|   {list[0]}   |   {list[1]}   |   {list[2]}   |   {list[3]}   |   {list[4]}   |   {list[5]}   |   {list[6]}   |   {list[7]}   |
+|   {list_of_entrances[0]}   |   {list_of_entrances[1]}   |   {list_of_entrances[2]}   |   {list_of_entrances[3]}   |   {list_of_entrances[4]}   |   {list_of_entrances[5]}   |   {list_of_entrances[6]}   |   {list_of_entrances[7]}   |
 |_______|_______|_______|_______|_______|_______|_______|_______|
 '''
     print(board)
@@ -36,10 +37,10 @@ def print_board():
 def enter_choice(player_choice, alternative):
     entrances = [-1, 7, 15, 23, 31, 39, 47]
     for i in entrances:
-        if list[int(player_choice) - 1] in [" "]:
-            list[int(player_choice) - 1] = alternative 
-        elif list[int(player_choice) + i] in ["X", "O"] and list[int(player_choice) + (i+8)] in [" "]:
-            list[int(player_choice) + (i+8)] = alternative
+        if list_of_entrances[int(player_choice) - 1] in [" "]:
+            list_of_entrances[int(player_choice) - 1] = alternative 
+        elif list_of_entrances[int(player_choice) + i] in ["X", "O"] and list_of_entrances[int(player_choice) + (i+8)] in [" "]:
+            list_of_entrances[int(player_choice) + (i+8)] = alternative
             break
       
 def game():
@@ -68,7 +69,7 @@ def game():
 
     attempts = 0
     while attempts >= 0:
-
+        attempts += 1
         if attempts % 2 != 0:
             player_choice = choice(player_one, "X")
             winner = determine_winner(player_one, player_two, tie)
@@ -99,30 +100,30 @@ def game():
 def determine_winner(player_one, player_two, tie):
 #   winner condition for horizontal rows
     for i in [1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 25, 26, 27, 28, 29, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45]:
-        if   list[i-1:i+3] == ['X', 'X', 'X', 'X']:
+        if   list_of_entrances[i-1:i+3] == ['X', 'X', 'X', 'X']:
             return player_one
-        elif list[i-1:i+3] == ['O', 'O', 'O', 'O']:
+        elif list_of_entrances[i-1:i+3] == ['O', 'O', 'O', 'O']:
             return player_two
 #   winner condition for vertical rows
     for i in [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]:
-        if   [list[i], list[i+8], list[i+16], list[i+24]] == ['X', 'X', 'X', 'X']:
+        if   [list_of_entrances[i], list_of_entrances[i+8], list_of_entrances[i+16], list_of_entrances[i+24]] == ['X', 'X', 'X', 'X']:
             return player_one
-        elif [list[i], list[i+8], list[i+16], list[i+24]] == ['O', 'O', 'O', 'O']:
+        elif [list_of_entrances[i], list_of_entrances[i+8], list_of_entrances[i+16], list_of_entrances[i+24]] == ['O', 'O', 'O', 'O']:
             return player_two
 #   winner condition for right diagonal
     for i in [0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 25, 26, 27, 28, 29, 33, 34, 35, 36]:
-        if   [list[i], list[i+9], list[i+18], list[i+27]] == ['X', 'X', 'X', 'X']:
+        if   [list_of_entrances[i], list_of_entrances[i+9], list_of_entrances[i+18], list_of_entrances[i+27]] == ['X', 'X', 'X', 'X']:
             return player_one
-        elif [list[i], list[i+9], list[i+18], list[i+27]] == ['O', 'O', 'O', 'O']:
+        elif [list_of_entrances[i], list_of_entrances[i+9], list_of_entrances[i+18], list_of_entrances[i+27]] == ['O', 'O', 'O', 'O']:
             return player_two
 #   winner condition for left diagonal
     for i in [3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 27, 28, 29, 30, 31, 35, 36, 37, 38, 39]:
-        if   [list[i], list[i+7], list[i+14], list[i+21]] == ['X', 'X', 'X', 'X']:
+        if   [list_of_entrances[i], list_of_entrances[i+7], list_of_entrances[i+14], list_of_entrances[i+21]] == ['X', 'X', 'X', 'X']:
             return player_one
-        elif [list[i], list[i+7], list[i+14], list[i+21]] == ['O', 'O', 'O', 'O']:
+        elif [list_of_entrances[i], list_of_entrances[i+7], list_of_entrances[i+14], list_of_entrances[i+21]] == ['O', 'O', 'O', 'O']:
             return player_two
 #   tie condition
-    while " " not in list:
+    while " " not in list_of_entrances:
         return tie
 
 def choice(player, alternative):
@@ -130,7 +131,7 @@ def choice(player, alternative):
     while player_choice not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
             player_choice = input(f"Invalid choice. Enter the column, {player}: ")
 
-    if list[int(player_choice) + 55] in ["X", "O"]:
+    if list_of_entrances[int(player_choice) + 55] in ["X", "O"]:
         print(f"This column is full, {player}. Choose another column.")
         choice(player, alternative)      
     enter_choice(player_choice, alternative)
